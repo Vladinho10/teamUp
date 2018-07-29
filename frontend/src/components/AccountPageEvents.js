b6607f315c1650436b7cbed22c4a49e831a5b726
Hovhannes Petrosyan <hovhannes.petrosyan95@gmail.com>
2018-07-29 18:59:08 +0400

    user profile page(80% done)

--- frontend/src/components/AccountPageEvents.js
+++ frontend/src/components/AccountPageEvents.js
@@ -2,58 +2,80 @@ import React,{Component} from 'react';
 import moment from 'moment';
 export default class AccountPageEvents extends Component{
     state = {
-
+        switcher:this.props.switcher
     }
-   
+    static getDerivedStateFromProps=(props,state)=>{ 
+      return {switcher:props.switcher};
+    }
     componentDidMount = () =>{
-        console.log(this.props.id);
         fetch('/api/profile/' + this.props.id,{
           credentials:'include',
           method:'GET'
         }).then((res)=>{return res.json()}).then((profile) => {
             console.log(profile)
-            let events = profile.user[0].own_events.concat(profile.user[0].attending_events);
+            let events = profile.user[0].own_events;
             console.log(events);
             fetch('/api/events/profile_events$'+ this.props.id,{
                 credentials:'include',
                 method:'GET'
             }).then((res)=>{return res.json()}).then((data) =>{
                 this.setState({
-                    events:data.events
+                    own_events:data.own_events,
+                    attending_events:data.attending_events,
+                    switcher:'admin'
                 });
             });
           
         });
       }
     render(){
-        if(this.state.events){
-            return this.state.events.map((el,i) => {
+        console.log(this.state,'545454545450');
+        if(this.state.own_events || this.state.attending_events ){
+            console.log('boommmmmmmmm');
+            let events = [];
+            if(this.state.switcher == 'admin'){
+                 events = this.state.own_events;
+                console.log(events,'boom');
+            }else{
+                 events = this.state.attending_events;
+            }
+            return events.map((el,i) => {
                 return(
-                    <div key={el._id} className='profile_event'>
-                        <div className = 'profile_event_left'>
-                            <div className='profile_event_img'>
-                                <img src={el.photo} />
-                            </div>
-                            
-                            <div className = 'profile_event_title'>
-                                {el.title}
-                            </div>
-                        </div>
-                        <div className = 'profile_event_right'>
-                            <div className = 'profile_event_day'>
-                                {moment(new Date(el.date)).format('LL')}
-                            </div>
-                            <div className='profile_event_time'>
-                               <img src="./images/clock.png" alt="" height='20' width='20'/> {el.time.split(":")[0] + ' : ' + el.time.split(":")[1] }
-                            </div>
-                            <div className='profile_event_loc'>
-                                <img src="./images/ev_ic.png" alt="" height='45' width='45'/>{el.location}
-                            </div>
-                            <div className='profile_event_type'>
-
+                    <div className = 'profile_event_frame' key ={el._id} >
+                        <div className = 'profile_event_img_frame'>
+                            <div className = 'profile_event_img'>
+                                <img src={el.photo} alt=""/>
+                                <div className='join_btn'>
+                                    <button className='profile_event_btn'>JOIN</button>
+                                </div>
                             </div>
                         </div>
-                        
+                        <div className = 'profile_event_info_frame'>
+                            <div className = 'profile_event_info_header'>
+                                <div className = 'profile_event_title'>
+                                    {el.title}
+                                </div>
+                                <div className = 'profile_event_date'>
+                                    {moment(new Date(el.date)).format('LL').split(',')[0]}
+                                </div>
+                            </div>
+                            <div className = 'profile_event_info_footer'>
+                                <div className = 'profile_event_description'>
+                                    {el.description.slice(0,25)}
+                                </div>
+                                <div className = 'profile_event_under_footer'>
+                                <div>
+                                        {el.type}
+                                    </div>
+                                    <div>
+                                       <img src="./images/clock.png" height='20' width='20'/> {el.time}
+                                    </div>
+                                    <div>
+                                        <img src="./images/ev_loc.png" height='20' width='20'/>{el.location}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 )
             });
