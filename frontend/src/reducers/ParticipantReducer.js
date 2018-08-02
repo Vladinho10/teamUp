d99e4f0fdca3d9c1e8fd55e07456b6993efdb270
Norayr Ghukasyan <norayr.ghukasian@gmail.com>
2018-08-02 13:03:32 +0400

    test

--- frontend/src/reducers/ParticipantReducer.js
+++ frontend/src/reducers/ParticipantReducer.js
@@ -1,10 +1,11 @@
-export default (state = {}, action) => {
-  switch (action.type) {
-    case 'ADD_PARTICIPANT':
-      return state;
-    case 'DELETE_PARTICIPANT':
-      return state; // { ...state, ...action.participants_count };
-    default:
-      return state;
-  }
-};
+// export default (state = {}, action) => {
+//   console.log(state, 'state from participantsReducer');
+//   switch (action.type) {
+//     case 'ADD_PARTICIPANT':
+//       return state;
+//     case 'DELETE_PARTICIPANT':
+//       return state; // { ...state, ...action.participants_count };
+//     default:
+//       return state;
+//   }
+// };
